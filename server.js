3637bf1e23d8f7f0bb30e5e29bfbb4cd942ee7d7
jrbreeden <jrbreeden55@gmail.com>
2022-06-07 10:45:17 -0400

    Created Fruit Model

--- server.js
+++ server.js
@@ -7,3 +7,38 @@ const morgan = require("morgan")
 const methodOverride = require("method-override")
 const mongoose = require("mongoose")
 const path = require("path")
+
+
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+// Setup inputs for our connect function
+const DATABASE_URL = process.env.DATABASE_URL
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+
+  // Establish connection
+  mongoose.connect(DATABASE_URL, CONFIG)
+
+  // Events for when connection opens/disconnects/errors
+mongoose.connection
+.on("open", () => console.log("Connected to Mongoose"))
+.on("close", () => console.log("Disconnected from Mongoose"))
+.on("error", (error) => console.log(error))
+
+
+////////////////////////////////////////////////
+// Our Models
+////////////////////////////////////////////////
+// pull schema and model from mongoose
+// const Schema = mongoose.Schema
+// const model = mongoose.model
+// is equal to:
+const { Schema, model} = mongoose
+
+// make fruits schema
+const Fruit = model("Fruit", fruitsSchema)
+
+  
